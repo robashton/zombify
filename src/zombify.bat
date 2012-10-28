d78b88aa74fe00dd4caa843c442f9c6b2f1290d6
Rob Ashton <robashton@codeofrob.com>
2012-10-28 15:56:47 +0100

    Added moar coffeescript

--- src/zombify.bat
+++ src/zombify.bat
@@ -1,6 +1,15 @@
+@ECHO OFF
+
+SET ZOMBIFY_PATH=%~dp0
+SET NODE_PATH=%ZOMBIFY_PATH%node_modules
+
 ECHO Set NODE_PATH to %NODE_PATH%
 
-ECHO Running tests with mocha
-PUSHD "C:\Users\Rob Ashton\Documents\GitHub\zombify2\Hotelier\Hotelier.Test" & "%ZOMBIFY_PATH%\node_modules\.bin\mocha.cmd" & POPD
+IF %1.==. (SET WORKING_DIRECTORY="%CD%") ELSE (SET WORKING_DIRECTORY="%1")
+
+ECHO Set working directoy to %WORKING_DIRECTORY%
+ECHO Running tests with mocha in that directory
+
+PUSHD "%WORKING_DIRECTORY%" & "%ZOMBIFY_PATH%node_modules\.bin\mocha.cmd" & POPD
 
 ECHO Finished running tests
